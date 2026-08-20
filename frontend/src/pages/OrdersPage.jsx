6c8b6f04897b7c8d837e078a3fb31195939d667f
adityabhore49-eng <adityabhore49@gmail.com>
2026-08-20 12:47:57 +0530

    order

--- frontend/src/pages/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage.jsx
@@ -5,25 +5,8 @@ const OrdersPage = () => {
     <div className="container w-100 h-screen bg-slate-800">
 
 
-      <div >
-        <div className="box h-30 text-white bg-gray-400 ">
-
-          <button>Add Order</button>
-          
-          </div>
-
-          <div>
-
-         <table>
-          <thead>
-            <tr>
-              <th></th>
-            </tr>
-          </thead>
-         </table>
-
-
-          </div>
+      <div className='card bg-white'>
+       
       </div>
 
 
