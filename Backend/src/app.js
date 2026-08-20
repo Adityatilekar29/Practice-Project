6f6a68c4f1b8d293903aa95a329aecad0a82b69b
Adityatilekar29 <tilekaraditya80@gmail.com>
2026-08-20 13:52:29 +0530

    add database connection

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const connectionDB = require("./DB/db")
+
 
 const app = express();
+
+connectionDB();
 app.use(cors());
+
+
+
 module.exports = app 
